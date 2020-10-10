d565a6a92fde0818d8d20823b146db99fafaa0b4
Simeon-Phsta <71008690+Simeon-Phsta@users.noreply.github.com>
2020-10-10 18:23:28 +0200

    en cours de réécriture des notes

--- Vue/vueCiivim.php
+++ Vue/vueCiivim.php
@@ -8,8 +8,68 @@
   <div class="row contenu">
     <section class="col-md-12 Nom">
       <p>
-        Siméon Philippe Saint-Antoine
-      </p>
+        Le Ciivim de Nevers
+
+        J'ai pu assisté à la 3ème édition du Siivim de Nevers, le vendredi 9 octobre.
+        Le Sommet international de l’innovation en villes médianes (SIIViM) se compose
+        de diverses exposants et d'échanges comme des pech ou des conférences par exemple.
+        Tout cela tourne autour de l'inovation numérique ou non afin d'améliorer le quotidient
+        des villes médianes et de leurs citoyens. 
+
+        Les inovations allaient de la réorganisation de nos façons de gérer les déchets en passant par
+        la création de plateforme pour la mise en relation de particulier avec des producteurs locaux pour
+        finir sur la façon de repenser nos objets connectés, de notre portable au data center pour les rendrent
+        moins polluant.
+
+        Pour ma part, je me suis concentré sur les projets et les échanges qui avaient pour but de recréer du lien social,
+        de revaloriser les centre-villes.
+
+        Pour commencer, les projets.
+
+
+
+        Natural Connect 
+        https://naturalidees.com/
+        4 associés 
+
+        Création d'une place de marché et d'un réseau social de proximité
+        Reproduire l'IRL en virtuel 
+        But de recréer du lien malgré le covid puis de la fidélité avec leur réseau social 
+        Le réseau social permet un échange entre clients à propos de commercants, de faire des commandes groupé
+
+        Ils proposent aussi la création de lieux de vie IRL qui peuvent prendre différentes formes 
+        selon les besoins du client (collectivités ou commerçants)
+        cela peut aussi bien être un lieux de récupération de commandes, de points d'informations,
+        de covoituring ou encore de parking à vélo sécurisé.
+
+        Ils ne prennent pas de stagiaire.
+
+
+
+        Cap Collectif 
+        https://cap-collectif.com/
+        Start up parisienne 
+
+        création de plateforme participative en livrant un cms ou alors font les pages pour la collectivités
+        C'est un SAS
+        le site est donc personalisable avec divers modules:
+        - permettre la proposition de projets au près de la commune pour ensuite  retenir des projets pour
+        qu'ils soient financancés par cette commune.
+        - un système d'actualité sous forme de blog
+        - montrer les évènements en ville avec un système d'inscription
+        - de mettre en avant les démarches participatives
+        - faire des sondages au près de leurs citoyens
+
+        pour un stage aurelien@cap-collectif.com en disant qu'on vient de la part de Maxime
+
+
+        Somei 
+        https://www.somei.fr/
+
+
+
+
+        </p>
     </section>
 
 
